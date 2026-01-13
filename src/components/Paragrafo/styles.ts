9c6a47ce24d338ae41ff88d98ebfed018228223d
Julia Benachio <julia.benachio@gmail.com>
2026-01-13 20:12:49 -0300

    Estilizando a seção de temas (projeto pendente de correção)

--- src/components/Paragrafo/styles.ts
+++ src/components/Paragrafo/styles.ts
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
+
 import { Props } from '.';
 
 export const P = styled.p<Props>`
   font-size: ${(props) => (props.fontSize ? props.fontSize + 'px' : '14px')};
-  color:${(props) => (props.tipo === 'principal' ? '#282a35' : '#949494')};
+  color: ${(props) =>
+    props.tipo === 'principal' ? props.theme.corPrincipal : '#949494'};
   line-height: 22px;
 `
---
+++ src/themes/light.ts
@@ -0,0 +1,10 @@
+const temaLight = {
+  corPrincipal: '#282a35',
+  corSecundaria: '#949494',
+  corDeFundo: '#fff',
+  corDeFundoDoBotao: '#4476bf',
+  corDaBorda: '#C1C1C1',
+}
+
+export default temaLight;
+
